206dc3d62889db0b7d7194e379f1a1ce0c78e646
Maksuedson <maksuedson.silva@ntconsult.com.br>
2022-08-15 17:02:11 -0300

    Adicionando servico que lista todos os clientes

--- src/main/java/com/projetomc/services/ClienteService.java
+++ src/main/java/com/projetomc/services/ClienteService.java
@@ -1,8 +1,7 @@
 package com.projetomc.services;
 
+import java.util.List;
 import java.util.Optional;
-
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +16,10 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 
+	public List<Cliente> listaClientes(){
+		return repo.findAll();
+	}
+	
 	public Cliente buscar(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObejctNotFoundException(
